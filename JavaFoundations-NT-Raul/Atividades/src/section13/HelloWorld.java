ec731fe48c0e23fe04623880ddc97e399c1675ad
farolfa <raulgoncalves.profissional@gmail.com>
2025-05-21 21:46:38 -0300

    uppando pelo proprio github mui dificil
    tai

---
+++ JavaFoundations-NT-Raul/Atividades/src/section13/HelloWorld.java
@@ -0,0 +1,11 @@
+package section13;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+    	
+        System.out.println("Hello World!");
+        System.out.println("tome-lhe semen quase que eu me engasgo com penis !!! gustavão bola bola penis no gustavão receba");
+        
+    }
+}
